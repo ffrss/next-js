39970733321f9ed541380a6554bf30db837dce98
R <radush17@icloud.com>
2023-10-11 21:45:45 +0300

    portfolio & category

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import Link from "next/link";
+import styles from "./page.module.scss";
 
 const Portfolio = () => {
-  return <div>Portfolio</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.selectTitle}>Choose a gallery</h1>
+      <div className={styles.items}>
+        <Link href="/portfolio/illustrations" className={styles.item}></Link>
+        <Link href="/portfolio/websites" className={styles.item}></Link>
+        <Link href="/portfolio/application" className={styles.item}></Link>
+      </div>
+    </div>
+  );
 };
 
 export default Portfolio;
